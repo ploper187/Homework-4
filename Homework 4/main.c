f3d2e3630067d2fe1bae95004e351cefaecbe7fa
Adam <adamthk@gmail.com>
2019-04-15 16:39:11 -0400

    Initial Commit

---
+++ Homework 4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Homework 4
+//
+//  Created by Adam on 4/15/19.
+//  Copyright © 2019 Adam Kuniholm. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
